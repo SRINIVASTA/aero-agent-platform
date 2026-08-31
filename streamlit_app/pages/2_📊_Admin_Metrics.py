8c68f9d91ae5fcf94265597950bc5f03ce112fd2
T A Srinivas <tasrinivass@gmail.com>
2026-08-31 23:24:39 +0530

    Update Admin Metrics script to set project path
    Add path manipulation to recognize root project workspace.

--- streamlit_app/pages/2_📊_Admin_Metrics.py
+++ streamlit_app/pages/2_📊_Admin_Metrics.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Explicitly force Python to recognize the root project workspace folder
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
